9dc03e3efa319ef8c47de75033c9c5f86b108178
Sevdiyor <diyor_1994_3838@mail.ru>
2015-06-18 18:00:23 +0500

    change index.html

---
+++ app/models/Teacher.scala
@@ -0,0 +1,19 @@
+package models
+
+/**
+ * Created by Sawyer on 17.06.2015.
+ */
+
+
+case class Teacher(id: Option[Int],
+                        name:String,
+                        surname:String,
+
+                        day:String,
+                        month:String,
+                        year:String,
+
+                        passportSeries:String,
+                        role:String,
+                        department:String,
+                         livingPlace:String)
