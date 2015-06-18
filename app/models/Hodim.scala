ab4c3f10b0a432384bbd556975fb1cfba3ec2e48
DostonAkhmedov <akhmedovdoston@gmail.com>
2015-06-18 20:26:00 +0500

    added Hodim medel

---
+++ app/models/Hodim.scala
@@ -0,0 +1,30 @@
+package models
+
+/**
+ * Created by Doston on 18.06.2015.
+ */
+
+case class Hodim(id: Option[Int],
+                First_name: String,
+                Last_name: String,
+                Second_name: String,
+                Department: String,
+                Boss: String)
+
+//class HodimTable(tag: Tag) extends  Table[Hodim](tag, "Hodimlar"){
+//
+//  def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
+//
+//  def First_name = column[String]("FIRST_NAME", O.Default(""))
+//
+//  def Last_name = column[String]("LAST_NAME", O.Default(""))
+//
+//  def Second_name = column[String]("SECOND_NAME", O.Default(""))
+//
+//  def Department = column[String]("DEPARTMENT", O.Default(""))
+//
+//  def Boss = column[String]("BOSS", O.Default(""))
+//
+//  def * = (id.?, First_name, Last_name, Second_name, Department, Boss) <> (Hodim.tupled, Hodim.unapply _)
+//
+//}
